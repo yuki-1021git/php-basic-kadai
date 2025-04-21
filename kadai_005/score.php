d9f16be660b1e36505ba719e6228ec2d20a1edca
yuki-1021git <escape.1021@outlook.com>
2025-04-21 21:58:10 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,16 @@
+<?php
+$score01 = 80;
+$score02 = 60;
+$score03 = 55;
+$score04 = 40;
+$score05 = 100;
+$score06 = 25;
+$score07 = 80;
+$score08 = 95;
+$score09 = 30;
+$score010 =60;
+
+$sum = $score01 + $score02 + $score03 + $score04 + $score05 + $score06 + $score07 + $score08 + $score09 + $score010;
+$average = $sum / 10;
+echo $average;
+?>
